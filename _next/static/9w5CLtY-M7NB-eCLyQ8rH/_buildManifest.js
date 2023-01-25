4a36d2ca9ce19daac3a16813ebd92c831208e252
Mohsen Heidari <chapanh@iran.ir>
2023-01-25 15:19:06 +0330

    new style

---
+++ _next/static/9w5CLtY-M7NB-eCLyQ8rH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5f3789e756040804.js"],"/_error":["static/chunks/pages/_error-318abef9b4b1a04d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
